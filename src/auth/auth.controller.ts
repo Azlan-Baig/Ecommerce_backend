9ffbfe589b778006c6a6fff9716556c1baca62a5
Azlan Baig <135128027+Azlan-Baig@users.noreply.github.com>
2025-03-27 15:19:14 +0500

    fixes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,29 +1,22 @@
-import {
-  Body,
-  Controller,
-  Headers,
-  Post,
-  Res,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Post, Req, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 
 @Controller('api/auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() dto: AuthDto, @Res() res : Response) {
+  signup(@Body() dto: AuthDto, @Res() res: Response) {
     return this.authService.signup(dto, res);
   }
   @Post('login')
-  login(@Body() dto: AuthDto) {
-    return this.authService.login(dto);
+  login(@Body() dto: AuthDto, @Res() res: Response) {
+    return this.authService.login(dto, res);
   }
-    @Post('logout')
-  async logout(@Headers('authorization') authHeader: string) {
-    return this.authService.logout(authHeader);
+  @Post('logout')
+  async logout(@Req() req: Request, @Res() res: Response) {
+    return this.authService.logout(req, res);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,15 @@
-import { HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto/auth.dto';
 import * as bcrypt from 'bcrypt';
 import { ConfigService } from '@nestjs/config';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 @Injectable()
 export class AuthService {
   constructor(
@@ -13,75 +18,90 @@ export class AuthService {
     private config: ConfigService,
   ) {}
 
-  async signup(dto: AuthDto, res : Response) {
-    const existingUser = await this.prisma.user.findUnique({
-      where: { email: dto.email },
-    });
-    if (existingUser) {
-      throw new HttpException('User Already exists', HttpStatus.BAD_REQUEST);
-    }
-    const hashedPassword = await bcrypt.hash(dto.password, 10);
-
-    const user = await this.prisma.user.create({
-      data: {
-        name: dto.name,
-        email: dto.email,
-        password: hashedPassword,
-      },
-    });
-    const tokens = await this.generateTokens(user.id, user.email);
-    await res.cookie('refreshToken', tokens.refreshToken, {
-      httpOnly: true,
-      secure : true,
-      sameSite : 'none',
-      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
-    });
-    return res.status(201).json({
-      message: 'User registered successfully',
-      accessToken: tokens.accessToken,
-    });
-  }
-
-  async login(dto: AuthDto) {
-    const user = await this.prisma.user.findUnique({
-      where: { email: dto.email },
-    });
-    if (!user) {
-      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
-    }
-    const isPasswordValid = await bcrypt.compare(dto.password, user.password);
-    if (!isPasswordValid) {
-      throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
-    }
-    const tokens = await this.generateTokens(user.id, user.email);
-    return {
-      message: 'User logged in successfully',
-      accessToken: tokens.accessToken,
-      refreshToken: tokens.refreshToken,
-    };
-  }
-  async logout(authHeader: string) {
-    if (!authHeader) {
-      throw new UnauthorizedException('Token is missing');
-    }
-
-    const token = authHeader.replace('Bearer ', '');
+  async signup(dto: AuthDto, res: Response) {
     try {
-      const payload = this.jwtService.verify(token, {
-        secret: process.env.ACCESS_SECRET,
+      const existingUser = await this.prisma.user.findUnique({
+        where: { email: dto.email },
       });
+      if (existingUser) {
+        throw new HttpException('User Already exists', HttpStatus.BAD_REQUEST);
+      }
+      const hashedPassword = await bcrypt.hash(dto.password, 10);
 
-      await this.prisma.user.update({
-        where: { id: payload.sub },
-        data: { refreshToken: null },
+      const user = await this.prisma.user.create({
+        data: {
+          name: dto.name,
+          email: dto.email,
+          password: hashedPassword,
+        },
       });
-
-      return { message: 'User logged out successfully' };
+      const tokens = await this.generateTokens(user.id, user.email);
+      await res.cookie('refreshToken', tokens.refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      });
+      return res.status(201).json({
+        message: 'User registered successfully',
+        accessToken: tokens.accessToken,
+      });
     } catch (error) {
-      throw new UnauthorizedException('Invalid or expired token');
+      throw new HttpException(
+        'Something went wrong',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
-  
+
+  async login(dto: AuthDto, res: Response) {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: { email: dto.email },
+      });
+      if (!user) {
+        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+      }
+      const isPasswordValid = await bcrypt.compare(dto.password, user.password);
+      if (!isPasswordValid) {
+        throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
+      }
+      const tokens = await this.generateTokens(user.id, user.email);
+
+      await res.cookie('refreshToken', tokens.refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      });
+      return res.status(201).json({
+        message: 'User logged-in successfully',
+        accessToken: tokens.accessToken,
+      });
+    } catch (error) {
+      throw new HttpException(
+        'Something went wrong',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+  async logout(req: Request, res: Response) {
+    try {
+      await res.clearCookie('refreshToken', {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+      });
+
+      return res.status(200).json({ message: 'User logged out successfully' });
+    } catch (error) {
+      throw new HttpException(
+        'Something went wrong',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
   async generateTokens(userId: string, email: string) {
     const payload = { sub: userId, email };
     const accessToken = this.jwtService.sign(payload, {
