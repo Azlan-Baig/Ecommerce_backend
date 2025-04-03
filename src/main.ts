b09fb505f6bcf679f3c0389d1397da018e7a2c31
Azlan Baig <135128027+Azlan-Baig@users.noreply.github.com>
2025-04-03 18:54:27 +0500

    wip

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { env } from 'process';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log(process.env.CLIENT_URL);
+  
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], 
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  })
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
